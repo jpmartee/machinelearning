a74e3e2a3b70dcc2709bd1c138da956212f3d38e
Justin Martinez <jpmartee@gmail.com>
2015-09-29 13:10:51 -0700

    Accuracy and Features for decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -14,20 +14,22 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+clf = DecisionTreeClassifier(min_samples_split=40)
+clf = clf.fit(features_train, labels_train)
 
+pred = clf.predict(features_test)
 
+acc = accuracy_score(pred, labels_test)
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
+print "accuracy: " + str(acc)
+print "no. of features: " + str(len(features_train[0]))
 
 
