434ffaec6d2e051594b3018646df30dd85900ce9
Justin Martinez <jpmartee@gmail.com>
2015-09-29 11:44:20 -0700

    Solved Naive Bayes and SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -19,13 +19,40 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
+# features_train = features_train[:1*len(features_train)/100]
+# labels_train = labels_train[:1*len(labels_train)/100]
 
 #########################################################
-### your code goes here ###
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(kernel='rbf', C=10000)
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+
+acc = accuracy_score(pred, labels_test)
+print acc
+
+print 'prediction of 10th element:',
+print pred[10]
+
+print 'prediction of 26th element:',
+print pred[26]
+
+print 'prediction of 50th element:',
+print pred[50]
+
+print 'There are ' + str(sum(pred)) + ' emails sent by Chris.'
+
 
 #########################################################
 
