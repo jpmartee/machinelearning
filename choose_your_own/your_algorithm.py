378d2fa58eea1c98a1bb7dee8028942baa20b4d9
Justin Martinez <jpmartee@gmail.com>
2015-10-01 17:24:48 -0700

    Completed choose your own exercise

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -3,6 +3,11 @@
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
@@ -27,15 +32,27 @@ plt.ylabel("grade")
 plt.show()
 #################################################################################
 
-
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
 
+#clf = AdaBoostClassifier(algorithm='SAMME')
+#clf = clf.fit(features_train, labels_train)
+#pred_ab = clf.predict(features_test)
+#acc_ab = accuracy_score(pred_ab, labels_test)
 
+#clf_rf = RandomForestClassifier(min_samples_split=50)
+#clf_rf = clf_rf.fit(features_train, labels_train)
+#pred_rf = clf_rf.predict(features_test)
+#acc_rf = accuracy_score(pred_rf, labels_test)
 
+clf_kn = KNeighborsClassifier(n_neighbors=8)
+clf_kn = clf_kn.fit(features_train, labels_train)
+pred_kn = clf_kn.predict(features_test)
+acc_kn = accuracy_score(pred_kn, labels_test)
 
-
-
+#print 'AdaBoost acc: ' + str(acc_ab)
+#print 'RandomForest acc: ' + str(acc_rf)
+print 'KNN acc: ' + str(acc_kn)
 
 
 try:
