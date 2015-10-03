9fb1e81a13b5c2c3d5ff0577a8e0dbb92bbd4a6f
Justin Martinez <jpmartee@gmail.com>
2015-10-02 17:27:55 -0700

    Created outlier cleaner to remove 10% worst data points

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -13,8 +13,19 @@ def outlierCleaner(predictions, ages, net_worths):
     
     cleaned_data = []
 
-    ### your code goes here
+    ### explore training data thinking of each point as tuple (pred(ages_train), ages_train, net_worths_train)
+    error = []
+    for i in range(len(ages)):
+        ### for each tuple compare pred(ages_train) and net_worths_train
+        diff = predictions[i] - net_worths[i]
+        error.append(abs(diff))
+        tup = ages[i], net_worths[i], diff
+        cleaned_data.append(tup)
 
-    
+    ### order the tuples by abs(pred(ages_train) - net_worths_train)
+    ### collect the first 90% of the tuples
+    cleaned_data.sort(key=lambda tup: tup[2])
+    cleaned_data = cleaned_data[:int(0.9*(len(cleaned_data)))]
+
     return cleaned_data
 
