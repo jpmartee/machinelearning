9c06b93c01a2ae9502864c5aa6f137cdc22d488f
Justin Martinez <jpmartee@gmail.com>
2015-10-11 21:59:19 -0700

    Performed train/test split and decision tree

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -14,6 +14,9 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split 
+from sklearn.metrics import accuracy_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,9 +27,16 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+print accuracy_score(pred, labels_test)
 
 
 
-### it's all yours from here forward!  
+ 
 
 
