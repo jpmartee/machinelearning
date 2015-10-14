86d83e16d8215c9f3257ba32759f18d1ea4d1c64
Justin Martinez <jpmartee@gmail.com>
2015-10-13 23:22:36 -0700

    Finished evaluation practice

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -16,6 +16,11 @@ import pickle
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.cross_validation import train_test_split 
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,9 +29,13 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
+clf = DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
-
-### your code goes here 
-
-
+print '\nNo. of POIs predicted in test set:', sum(pred)
+print '\nNo. of people in test set:', len(pred)
+print '\nPrecision score of POI identifier:', precision_score(pred, labels_test)
+print '\nRecall score of POI identifier:', recall_score(pred, labels_test)
